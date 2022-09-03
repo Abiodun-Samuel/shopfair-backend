31c20fca8c0e1c5fe35a1a7863b8dbee3f323503
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2022-09-03 10:53:50 +0100

    deployment prep to vercel

--- server.js
+++ server.js
@@ -57,7 +57,9 @@ app.all("*", (req, res) => {
 
 mongoose.connection.once("open", () => {
   console.log("Connected to MongoDB");
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  app.listen(process.env.PORT || PORT, () =>
+    console.log(`Server running on port ${PORT}`)
+  );
 });
 
 mongoose.connection.on("error", (err) => {
